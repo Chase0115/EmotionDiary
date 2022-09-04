52cefaabe5cc0bf240eaced4ea359a65ae2a02e1
chase0115 <devchase0115@gmail.com>
2022-09-04 23:05:43 +1000

    Component TodayDiary add error handling function

--- src/components/todayDiary.jsx
+++ src/components/todayDiary.jsx
@@ -1,30 +1,54 @@
 import React from "react";
 import "./todayDiary.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const TodayDiary = () => {
   const [state, setState] = useState({
     title: "",
     description: "",
     score: "3",
-  })
+  });
+
+  const titleInput = useRef();
+  const description = useRef();
 
   const onChange = (e) => {
     setState({
       ...state,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (state.title.length < 1) {
+      titleInput.current.focus();
+      return;
+    }
+
+    if (state.description.length < 5) {
+      description.current.focus();
+      return;
+    }
+
+    alert("Saved Today's Diary");
+  };
 
   return (
     <div className='todayDiary'>
       <h2>Today's Dirary</h2>
-      <form className='writeForm' onSubmit={(e) => {
-        e.preventDefault();
-        alert(`title: ${state.title} desc: ${state.description} score: ${state.score}`)
-      }}>
+      <form
+        className='writeForm'
+        onSubmit={(e) => {
+          onSubmit(e, state.description);
+          console.log(
+            `title: ${state.title} desc: ${state.description} score: ${state.score}`
+          );
+        }}
+      >
         <input
           className='title'
+          ref={titleInput}
           name='title'
           type='text'
           placeholder='Title'
@@ -33,6 +57,7 @@ const TodayDiary = () => {
         />
         <textarea
           className='description'
+          ref={description}
           name='description'
           cols='30'
           rows='10'
@@ -42,7 +67,12 @@ const TodayDiary = () => {
         />
         <label className='score' htmlFor='score'>
           How was today? :
-          <select className="selectBox" name='score' value={state.score} onChange={onChange}>
+          <select
+            className='selectBox'
+            name='score'
+            value={state.score}
+            onChange={onChange}
+          >
             <option value={1}>1</option>
             <option value={2}>2</option>
             <option value={3}>3</option>
