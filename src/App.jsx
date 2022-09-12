6f2f335a26f08b007d38ccbaafc606227d461929
chase0115 <devchase0115@gmail.com>
2022-09-12 21:28:21 +1000

    fetch API - how to use with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,34 @@ import "./App.css";
 import TodayDiary from "./components/todayDiary";
 import DiaryList from "./components/diaryList";
 import { useState } from "react";
+import { useEffect } from "react";
+
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [list, setList] = useState([]);
   const [id, setId] = useState(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    
+    const initData = res.slice(0,20).map((item) => {
+      return {
+        title: item.name,
+        description: item.body,
+        score: Math.floor(Math.random() * 5 ) + 1,
+        createdTime: new Date().getTime(),
+        id: item.id,
+      }
+    })
+    setList(initData)
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (title, score, description, createdTime) => {
     const newItem = { id, title, score, description, createdTime };
